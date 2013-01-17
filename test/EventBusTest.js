ceae9c3891e8a595a8ad1b40dec84ca84cfd3f87
taoyuan <taoyuan@evymind.com>
2013-01-17 23:18:53 +0800

    Use ovy to define EventBus

--- test/EventBusTest.js
+++ test/EventBusTest.js
@@ -24,6 +24,18 @@ describe('EventBus', function(){
             expect(subscription.scope).to.eql(Accounts);
         });
 
+
+        it('#on', function() {
+            var bus = new EventBus();
+            bus.on('create_user', Accounts.handlers.create_user, Accounts);
+            var subscriptionArray = bus.subscriptions['create_user'];
+            expect(subscriptionArray).to.be.ok();
+            expect(subscriptionArray.length).to.be(1);
+            var subscription = subscriptionArray[0];
+            expect(subscription.handler).to.eql(Accounts.handlers.create_user);
+            expect(subscription.scope).to.eql(Accounts);
+        })
+
         it('#unsubscribe', function() {
             var bus = new EventBus();
             bus.subscribe('create_user', Accounts.handlers.create_user, Accounts);
@@ -37,7 +49,20 @@ describe('EventBus', function(){
             }
         });
 
-        it('dispatch', function(done){
+        it('#un', function() {
+            var bus = new EventBus();
+            bus.subscribe('create_user', Accounts.handlers.create_user, Accounts);
+            var subscriptionArray = bus.subscriptions['create_user'];
+            expect(subscriptionArray).to.be.ok();
+            expect(subscriptionArray.length).to.be(1);
+            bus.un('create_user', Accounts.handlers.create_user, Accounts);
+            subscriptionArray = bus.subscriptions['create_user'];
+            if (subscriptionArray) {
+                expect(subscriptionArray.length).to.be(0);
+            }
+        });
+
+        it('#dispatch', function(done){
             var bus = new EventBus();
             var user = {name: 'taoyuan', email: 'torworx@gmail.com', password: '123456'};
             bus.subscribe('create_user', Accounts.handlers.create_user);
@@ -49,5 +74,6 @@ describe('EventBus', function(){
                 }
             );
         });
+
     })
 })
